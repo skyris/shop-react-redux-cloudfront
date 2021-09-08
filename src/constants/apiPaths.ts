aafb13e496358a6d40e9fe2a1e62493c8e7859c8
Victor Klimov <4klimov@gmail.com>
2021-09-09 01:14:16 +0300

    refactor: change url for new database endpoint

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -5,7 +5,7 @@ const API_PATHS = {
   import: '',
   cart: '',
   bff: '',
-  products: 'https://mhxfvszlp9.execute-api.eu-north-1.amazonaws.com/dev'
+  products: 'https://ctefno09al.execute-api.eu-west-1.amazonaws.com/dev'
 };
 
 export default API_PATHS;
